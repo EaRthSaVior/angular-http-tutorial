2bc4db1b8e121034a4c1d0d05daf31549acc9766
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-09-08 13:05:25 +0800

    http post and get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
     console.log(postData);
+    this.http
+      .post<{ name: string }>(
+        'https://angular-http-tutorial-2a7b4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((response) => console.log(response));
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://angular-http-tutorial-2a7b4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+      )
+      .pipe(
+        map((response) => {
+          const arr = [];
+          for (let key in response) {
+            if (response.hasOwnProperty(key)) {
+              arr.push({ ...response[key], id: key });
+            }
+          }
+          return arr;
+        })
+      )
+      .subscribe((response) => (this.loadedPosts = response));
   }
 }
